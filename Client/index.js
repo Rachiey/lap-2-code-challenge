41dbead40f98c2ac343d96f07200737bc3efcc75
“Rachiey” <rachaelyonoff@gmail.com>
2021-10-20 19:09:36 +0100

    created static template for demonstration purposes

--- Client/index.js
+++ Client/index.js
@@ -41,7 +41,12 @@ submitButton.addEventListener('click', e => {
   submitPost(e);
 });
 
-
+//current date
+n =  new Date();
+y = n.getFullYear();
+m = n.getMonth() + 1;
+d = n.getDate();
+document.getElementById("date").textContent = m + "/" + d + "/" + y;
 
 // function makeDocument() {
 //     let frame = document.getElementById("theFrame");
