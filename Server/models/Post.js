54711ff293f74b3ebdcb54b78a13bc8d140003bc
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-10-20 12:29:06 +0100

    add model and methods to get all posts / individual post

---
+++ Server/models/Post.js
@@ -0,0 +1,46 @@
+const db = require('../dbConfig/init');
+
+module.exports = class Post {
+    constructor(data){
+        this.id = data.id;
+        this.title = data.title;
+        this.body = data.body;
+        this.date = data.date;
+        this.author = data.pseudonym;
+    };
+
+    static get all(){
+        return new Promise (async (res, rej) => {
+            try {
+                const postsData = await db.query(`SELECT posts.*, authors.pseudonym
+                                                    FROM posts 
+                                                    INNER JOIN authors on posts.author_id = authors.id;`);
+                const posts = postsData.rows.map(p => new Post(p));
+                res(posts);
+            } catch (err) {
+                console.log(err);
+                rej("Error retrieving posts");
+            }
+        })
+    }
+
+    static findById(id){
+        return new Promise (async (res, rej) => {
+            try {
+                const query = {
+                    text: `SELECT posts.*, authors.pseudonym
+                                FROM posts
+                                INNER JOIN authors ON posts.author_id = authors.id
+                                WHERE posts.id = $1;`,
+                    values: [ id ],
+                }
+                const postData = await db.query(query);
+                const post = new Post(postData.rows[0]);
+                res(post)
+            } catch (err) {
+                console.log(err);
+                rej("Error retrieving post");
+            }
+        })
+    }
+}
